54932e6a5ea7b40f97ebbb38924b281f9573daec
Israel Guillermo <learsi.guil2010@gmail.com>
2020-06-30 03:20:40 -0400

    Capas

--- src/app/components/module-geo/home/home.component.ts
+++ src/app/components/module-geo/home/home.component.ts
@@ -14,16 +14,10 @@ export class HomeComponent implements OnInit, AfterContentInit {
   ventanaLocalizacion = true;
   latitud = -16.6478;
   longitud = -68.2922;
-
   private subscription: Subscription;
   public message: any;
-  constructor(public wsService: WebsocketService, private mqttService: MqttService) {
-    this.subscription = this.mqttService.observe('LatLon').subscribe((message: IMqttMessage) => {
-      this.message = JSON.parse(message.payload.toString());
-      console.log(this.message);
-      L.marker([this.message.latitud, this.message.longitud]).addTo(this.map).bindPopup(this.message.sensorID + "--" + this.message.velocidad).openPopup();
-    });
-  }
+  // constructor(public wsService: WebsocketService, private mqttService: MqttService) { }
+  constructor(private mqttService: MqttService) { }
   ngOnInit() {
   }
   ngAfterContentInit(): void {
@@ -31,34 +25,47 @@ export class HomeComponent implements OnInit, AfterContentInit {
   }
 
   private initMap(): void {
-    this.map = L.map('mapa', {
-      center: [this.latitud, this.longitud],
-      zoom: 18
-    });
-    const carto = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png', {
-      subdomains: 'abcd',
-      maxZoom: 21,
-    });
     const options = {
       bingMapsKey: 'AnSERau4L-wtJSyhhTX8E7WmVMOft_yhaeAvftc9Vjo-ijbhH59sJmDeHa_aMS8v',
       imagerySet: 'Aerial',
-      maxZoom: 21
+      maxZoom: 20,
     };
     const bing = new bingTileLayer(options);
 
-    bing.addTo(this.map);
-    carto.addTo(this.map);
-  }
-  iniciar() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.latitud = position.coords.latitude;
-        this.longitud = position.coords.longitude;
-        this.map.panTo(new L.LatLng(this.latitud, this.longitud));
-        L.marker([this.latitud, this.longitud]).addTo(this.map);
-      });
-      this.ventanaLocalizacion = false;
+    const grafical = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+    });
+
+    const carto = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png', {
+      subdomains: 'abcd',
+      maxZoom: 18,
+    });
+
+    this.map = L.map('mapa', {
+      center: [this.latitud, this.longitud],
+      zoom: 18,
+      layers: [bing, grafical],
+    });
+
+    const baseMaps = {
+      'Planos': bing,
+      'Satelite': grafical,
+    };
+
+    const overlayMaps = {
+      'Cites': carto,
     }
 
+
+    L.control.layers(baseMaps, overlayMaps).addTo(this.map);
+  }
+  iniciar() {
+    this.ventanaLocalizacion = false;
+    this.subscription = this.mqttService.observe('LatLon').subscribe((message: IMqttMessage) => {
+      this.message = JSON.parse(message.payload.toString());
+      this.map.panTo(new L.LatLng(this.message.latitud, this.message.longitud));
+      // tslint:disable-next-line: max-line-length
+      L.marker([this.message.latitud, this.message.longitud]).addTo(this.map).bindPopup(this.message.sensorID + '--' + this.message.velocidad).openPopup();
+    });
   }
 }
