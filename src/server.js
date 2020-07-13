86095ea77feaa6af36843eee4fe79328553ac020
Israel Guillermo <learsi.guil2010@gmail.com>
2020-07-12 21:47:16 -0400

    Quinto Commit

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/geofront'));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/geofront/index.html'));
 });
